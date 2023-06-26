6b6bee7226a73ceafdfb232c5e4fa0b01fbea0e3
Jason Ballejo <ballejo19@gmail.com>
2023-06-26 11:14:41 -0400

    new header

--- component/Header.tsx
+++ component/Header.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 
 export default function Header() {
-  return (
-    <div>Test Header</div>
-  )
+return (
+    <div>
+        <h2>Test Header</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum voluptatem quos veniam rem nam excepturi voluptas aliquid praesentium vel et eos voluptatum beatae, dignissimos nostrum atque tempora ipsa optio! Ad!</p>
+    </div>
+)
 }
